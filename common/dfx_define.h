9f17a2e597181ff7646500441aa09c55f483c1b1
yanmengzhao <yanmengzhao1@huawei.com>
2022-03-11 18:06:21 +0800

    fix codestyle
    Signed-off-by: yanmengzhao <yanmengzhao1@huawei.com>

--- common/dfx_define.h
+++ common/dfx_define.h
@@ -15,48 +15,46 @@
 #ifndef DFX_DEFINE_H
 #define DFX_DEFINE_H
 
-#define BOOL int
-#define TRUE 1
-#define FALSE 0
-#define NAME_LEN 128
+static const int NAME_LEN = 128;
 
 #if defined(__arm__)
-#define USER_REG_NUM        16
-#define REG_PC_NUM          15
-#define REG_LR_NUM          14
+static const int USER_REG_NUM = 16;
+static const int REG_PC_NUM = 15;
+static const int REG_LR_NUM = 14;
 #elif defined(__aarch64__)
-#define USER_REG_NUM        34
-#define REG_PC_NUM          32
-#define REG_LR_NUM          30
+static const int USER_REG_NUM = 34;
+static const int REG_PC_NUM = 32;
+static const int REG_LR_NUM = 30;
 #elif defined(__x86_64__)
-#define USER_REG_NUM        27
-#define REG_PC_NUM          16
+static const int USER_REG_NUM = 27;
+static const int REG_PC_NUM = 16;
 #endif
 
-#define ARM_EXEC_STEP_NORMAL        4
-#define ARM_EXEC_STEP_THUMB         3
+static const int ARM_EXEC_STEP_NORMAL = 4;
+static const int ARM_EXEC_STEP_THUMB = 3;
 
-#define CONF_LINE_SIZE 1024
+static const int CONF_LINE_SIZE = 1024;
 
-#define FAULTSTACK_ITEM_BUFFER_LENGTH 1024
-#define FAULTSTACK_SP_REVERSE 3
-#define FAULTSTACK_FIRST_FRAME_SEARCH_LENGTH 64
+static const int FAULTSTACK_ITEM_BUFFER_LENGTH = 1024;
+static const int FAULTSTACK_SP_REVERSE = 3;
+static const int FAULTSTACK_FIRST_FRAME_SEARCH_LENGTH = 64;
+
 
 // max unwind 64 steps.
-#define BACK_STACK_MAX_STEPS    64
+static const int BACK_STACK_MAX_STEPS = 64;
 // 128K back trace stack size
-#define BACK_STACK_INFO_SIZE    (128 * 1024)
+static const int BACK_STACK_INFO_SIZE = 128 * 1024;
 
-#define SIGDUMP 35
+static const int SIGDUMP = 35;
 
-#define DUMP_CATCHER_NUMBER_ONE                 1
-#define DUMP_CATCHER_NUMBER_TWO                 2
-#define DUMP_CATCHER_SLEEP_TIME_TEN_S           10
-#define DUMP_CATCHER_SLEEP_TIME_TWENTY_S        20
-#define DUMP_CATCHER_WAIT_LOG_FILE_GEN_TIME_US        10000
-#define DUMP_CATCHE_WORK_TIME_S                 60
+static const int DUMP_CATCHER_NUMBER_ONE = 1;
+static const int DUMP_CATCHER_NUMBER_TWO = 2;
+static const int DUMP_CATCHER_SLEEP_TIME_TEN_S = 10;
+static const int DUMP_CATCHER_SLEEP_TIME_TWENTY_S = 20;
+static const int DUMP_CATCHER_WAIT_LOG_FILE_GEN_TIME_US = 10000;
+static const int DUMP_CATCHE_WORK_TIME_S = 60;
 
-#define NUMBER_TWO_KB                2048
+static const int NUMBER_TWO_KB = 2048;
 
 #if defined(TEMP_FAILURE_RETRY)
 #undef TEMP_FAILURE_RETRY
