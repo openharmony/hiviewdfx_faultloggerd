0d60b08289992add68099be9c93a2644dd92f2b1
yanmengzhao <yanmengzhao1@huawei.com>
2022-04-28 21:30:33 +0800

    refix fuzz cases
    Signed-off-by: yanmengzhao <yanmengzhao1@huawei.com>

--- test/fuzztest/faultloggerd_fuzzer/faultloggerd_fuzzer.cpp
+++ test/fuzztest/faultloggerd_fuzzer/faultloggerd_fuzzer.cpp
@@ -28,46 +28,44 @@ using namespace OHOS::HiviewDFX;
 using namespace std;
 
 static const int PID_SIZE = 4;
-static const int RAND_BUF_LIMIT = 28;
-static const int RAND_PARAM_SIZE = 20;
+static const int RAND_BUF_LIMIT = 9;
 
 namespace OHOS {
 bool DumpStackTraceTest(const uint8_t* data, size_t size)
 {
-    shared_ptr<DfxDumpCatcher> catcher = make_shared<DfxDumpCatcher>();
-    if (size >= RAND_BUF_LIMIT) {
-        string msg;
-        int pid[1];
-        int tid[1];
-        char randParam[RAND_PARAM_SIZE + 1] = { '\0' };
-        errno_t err = memcpy_s(pid, sizeof(pid), data, PID_SIZE);
-        if (err != EOK) {
-            cout << "DumpStackTraceTest :: memcpy_s pid failed" << endl;
-        }
-        data += PID_SIZE;
-        err = memcpy_s(tid, sizeof(tid), data, PID_SIZE);
-        if (err != EOK) {
-            cout << "DumpStackTraceTest :: memcpy_s tid failed" << endl;
-        }
-        data += PID_SIZE;
-        err = strncpy_s(randParam, sizeof(randParam), reinterpret_cast<const char*>(data), RAND_PARAM_SIZE);
-        if (err != EOK) {
-            cout << "DumpStackTraceTest :: strncpy_s randParam failed" << endl;
-        }
-        cout << "pid = " << pid[0] << " tid = " << tid[0] << " randParam = " << string(randParam) << endl;
-
-        if (catcher->DumpCatch(pid[0], tid[0], msg)) {
-            cout << msg << endl;
-        } else {
-            cout << "DumpStackTraceTest :: dumpcatch failed." << endl;
-        }
-
-        string processdumpCmd = "processdump -p " + to_string(pid[0]) + " -t " + to_string(tid[0]);
-        system(processdumpCmd.c_str());
-
-        string processdumpInvalidCmd = "processdump " + string(randParam);
-        system(processdumpInvalidCmd.c_str());
+    if (size < RAND_BUF_LIMIT) {
+        return true;
     }
+    shared_ptr<DfxDumpCatcher> catcher = make_shared<DfxDumpCatcher>();
+    string msg;
+    int pid[1];
+    int tid[1];
+    errno_t err = memcpy_s(pid, sizeof(pid), data, PID_SIZE);
+    if (err != EOK) {
+        cout << "DumpStackTraceTest :: memcpy_s pid failed" << endl;
+    }
+    data += PID_SIZE;
+    err = memcpy_s(tid, sizeof(tid), data, PID_SIZE);
+    if (err != EOK) {
+        cout << "DumpStackTraceTest :: memcpy_s tid failed" << endl;
+    }
+    data += PID_SIZE;
+    char invalidOption = *data;
+    cout << "pid = " << pid[0] << " tid = " << tid[0] << " invalidOpt = " << invalidOption << endl;
+
+    if (catcher->DumpCatch(pid[0], tid[0], msg)) {
+        cout << msg << endl;
+    } else {
+        cout << "DumpStackTraceTest :: dumpcatch failed." << endl;
+    }
+
+    string processdumpCmd = "processdump -p " + to_string(pid[0]) + " -t " + to_string(tid[0]);
+    system(processdumpCmd.c_str());
+
+    string processdumpInvalidCmd = "processdump -" + to_string(invalidOption) + " -p " +
+        to_string(pid[0]) + " -t " + to_string(tid[0]);
+    system(processdumpInvalidCmd.c_str());
+
     return true;
 }
 } // namespace OHOS
