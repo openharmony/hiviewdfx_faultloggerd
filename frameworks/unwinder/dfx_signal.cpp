bd5c28c2be96aa06dd800c41e56b6819040d1b7c
cs1111 <chenshi51@huawei.com>
2023-11-20 17:42:32 +0800

    fix problem of printing fault addr in some case
    Signed-off-by: cs1111 <chenshi51@huawei.com>

--- frameworks/unwinder/dfx_signal.cpp
+++ frameworks/unwinder/dfx_signal.cpp
@@ -79,9 +79,9 @@ std::string DfxSignal::PrintSignal(const siginfo_t &info)
     DfxSignal signal(info.si_signo);
     if (signal.IsAddrAvailable()) {
 #if defined(__LP64__)
-        sigString = sigString + StringPrintf("@%#016llx ", (uint64_t)info.si_addr);
+        sigString = sigString + StringPrintf("@%#018llx ", (uint64_t)info.si_addr);
 #else
-        sigString = sigString + StringPrintf("@%#08llx ", (uint64_t)info.si_addr);
+        sigString = sigString + StringPrintf("@%#010llx ", (uint64_t)info.si_addr);
 #endif
     }
 
