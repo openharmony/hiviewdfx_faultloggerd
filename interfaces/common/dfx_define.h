2df653fbe69c2d47e72012a8d32f013e03a92c81
xlgitee <xulong19@huawei.com>
2024-07-15 16:54:07 +0800

    add ptrace time
    Signed-off-by: xlgitee <xulong19@huawei.com>
    Change-Id: I020678bd1d34faf2ec52f57f4f6236754e07cf19

--- interfaces/common/dfx_define.h
+++ interfaces/common/dfx_define.h
@@ -41,7 +41,7 @@ static const int NUMBER_ONE_THOUSAND = 1000;
 static const int NUMBER_ONE_MILLION = 1000000;
 
 static const int PTRACE_ATTATCH_KEY_THREAD_TIMEOUT = 1000;
-static const int PTRACE_ATTATCH_OTHER_THREAD_TIMEOUT = 10;
+static const int PTRACE_ATTATCH_OTHER_THREAD_TIMEOUT = 50;
 
 static const int INVALID_FD = -1;
 static const int DUMP_TYPE_REMOTE = -1;
