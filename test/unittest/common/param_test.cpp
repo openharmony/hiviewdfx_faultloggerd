3ad4ce88a2d7152820b4755248fba4fc749fc301
wenlong_12 <wenlong12@huawei.com>
2024-08-19 21:32:26 +0800

    Test cases for abnormal scenarios
    Signed-off-by: wenlong_12 <wenlong12@huawei.com>

--- test/unittest/common/param_test.cpp
+++ test/unittest/common/param_test.cpp
@@ -16,6 +16,8 @@
 #include <gtest/gtest.h>
 #include "dfx_define.h"
 #include "dfx_param.h"
+#include "dfx_trace.h"
+#include "dfx_util.h"
 
 using namespace OHOS::HiviewDFX;
 using namespace testing::ext;
@@ -41,6 +43,38 @@ HWTEST_F(ParamTest, DfxParamTest001, TestSize.Level2)
     GTEST_LOG_(INFO) << "DfxParamTest001: start.";
     ASSERT_EQ(DfxParam::EnableMixstack(), true);
     GTEST_LOG_(INFO) << "DfxParamTest001: end.";
+}
+
+/**
+ * @tc.name: TraceTest001
+ * @tc.desc: test FormatTraceName functions
+ * @tc.type: FUNC
+ */
+HWTEST_F(ParamTest, TraceTest001, TestSize.Level2)
+{
+    GTEST_LOG_(INFO) << "TraceTest001: start.";
+    char *name = nullptr;
+    FormatTraceName(name, 0, nullptr);
+    ASSERT_EQ(name, nullptr);
+    const size_t size = 2;
+    FormatTraceName(name, size, nullptr);
+    ASSERT_EQ(name, nullptr);
+    GTEST_LOG_(INFO) << "TraceTest001: end.";
+}
+
+/**
+ * @tc.name: DfxutilTest001
+ * @tc.desc: test TrimAndDupStr functions
+ * @tc.type: FUNC
+ */
+HWTEST_F(ParamTest, DfxutilTest001, TestSize.Level2)
+{
+    GTEST_LOG_(INFO) << "DfxutilTest001: start.";
+    std::string filePath = "";
+    std::vector<const std::string> validPaths;
+    bool ret = VerifyFilePath(filePath, validPaths);
+    ASSERT_EQ(ret, true);
+    GTEST_LOG_(INFO) << "DfxutilTest001: end.";
 }
 } // namespace HiviewDFX
 } // namespace OHOS
