ff5868c48f7b6855ef6ec1d2a96490ddc9dbd21c
wangqing <wangqing195@huawei.com>
2024-09-03 09:04:45 +0800

    系统调用无权限时，新增log信息
    Signed-off-by: wangqing <wangqing195@huawei.com>
    Change-Id: I9824d478f086d1b3dfad5dc1c97051567907f581

--- interfaces/innerkits/unwinder/include/dfx_signal.h
+++ interfaces/innerkits/unwinder/include/dfx_signal.h
@@ -47,6 +47,7 @@ private:
     static std::string FormatSIGFPECodeName(const int32_t signalCode);
     static std::string FormatSIGSEGVCodeName(const int32_t signalCode);
     static std::string FormatSIGTRAPCodeName(const int32_t signalCode);
+    static std::string FormatSIGSYSCodeName(const int32_t signalCode);
     static std::string FormatCommonSignalCodeName(const int32_t signalCode);
 private:
     int32_t signal_ = 0;
