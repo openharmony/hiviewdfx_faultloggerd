eb3c4ff34f90e8b5314927e579c17ec3ec196853
xlgitee <xulong19@huawei.com>
2024-11-14 21:09:54 +0800

    add fd leak case
    Signed-off-by: xlgitee <xulong19@huawei.com>

--- test/systemtest/faultloggerd_system_test.cpp
+++ test/systemtest/faultloggerd_system_test.cpp
@@ -1674,5 +1674,41 @@ HWTEST_F(FaultLoggerdSystemTest, FaultLoggerdSystemTest125, TestSize.Level2)
     UninstallTestHap(TEST_BUNDLE_NAME);
     GTEST_LOG_(INFO) << "FaultLoggerdSystemTest125: end.";
 }
+
+/**
+* @tc.name: FaultLoggerdSystemTest126
+* @tc.desc: Test process fd leak unwind
+* @tc.type: FUNC
+*/
+HWTEST_F(FaultLoggerdSystemTest, FaultLoggerdSystemTest126, TestSize.Level2)
+{
+    GTEST_LOG_(INFO) << "FaultLoggerdSystemTest126: start.";
+    pid_t pid = fork();
+    if (pid < 0) {
+        EXPECT_GT(pid, -1);
+    } else if (pid == 0) {
+        int fd = 0;
+        while (true) {
+            fd = open("/dev/null", O_RDONLY);
+            if (fd == -1) {
+                raise(SIGABRT);
+                break;
+            }
+        }
+    } else {
+        sleep(2);
+        auto fileName = GetCppCrashFileName(pid, TEMP_DIR);
+        EXPECT_NE(0, fileName.size());
+        string log[] = {
+            "Pid:", "Uid", "SIGABRT", "Tid:", "#00",
+            "Registers:", REGISTERS, "FaultStack:", "Maps:"
+        };
+        int minRegIdx = 5; // 5 : index of first REGISTERS - 1
+        int expectNum = sizeof(log) / sizeof(log[0]);
+        int count = CheckKeyWords(fileName, log, expectNum, minRegIdx);
+        EXPECT_EQ(count, expectNum);
+        GTEST_LOG_(INFO) << "FaultLoggerdSystemTest126: end.";
+    }
+}
 } // namespace HiviewDFX
 } // namespace OHOS
