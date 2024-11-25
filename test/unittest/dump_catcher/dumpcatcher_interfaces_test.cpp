f0c606db07c1102c88a231948013f033540b29fa
cs1111 <chenshi51@huawei.com>
2024-11-25 11:18:55 +0800

    feature:get and format kernel stack
    Signed-off-by: cs1111 <chenshi51@huawei.com>
    Change-Id: I568319fe4aef65f1fc2065ded6715c156c06518b

--- test/unittest/dump_catcher/dumpcatcher_interfaces_test.cpp
+++ test/unittest/dump_catcher/dumpcatcher_interfaces_test.cpp
@@ -740,7 +740,7 @@ HWTEST_F(DumpCatcherInterfacesTest, DumpCatcherInterfacesTest030, TestSize.Level
 
 /**
  * @tc.name: DumpCatcherInterfacesTest031
- * @tc.desc: test DumpCatchProcess
+ * @tc.desc: test DumpCatchProcess get kenerl stack
  * @tc.type: FUNC
  */
 HWTEST_F(DumpCatcherInterfacesTest, DumpCatcherInterfacesTest031, TestSize.Level2)
@@ -776,9 +776,18 @@ HWTEST_F(DumpCatcherInterfacesTest, DumpCatcherInterfacesTest031, TestSize.Level
     ASSERT_TRUE(DfxJsonFormatter::FormatKernelStack(msg, formattedStack, false));
     ASSERT_GT(formattedStack.size(), 0);
     GTEST_LOG_(INFO) << formattedStack;
-    ASSERT_NE(formattedStack.find("#"), std::string::npos);
+    string log[] = { "Tid:", "Name:", "#00", "/system/bin/appspawn", "Name:OS_DfxWatchdog" };
+    log[0] += std::to_string(g_testPid);
+    log[1] += TRUNCATE_TEST_BUNDLE_NAME;
+    int len = sizeof(log) / sizeof(log[0]);
+    int count = GetKeywordsNum(formattedStack, log, len);
+    EXPECT_EQ(count, len) << formattedStack << "DumpCatcherInterfacesTest031 Failed";
     ASSERT_TRUE(DfxJsonFormatter::FormatKernelStack(msg, formattedStack, true));
-    GTEST_LOG_(INFO) << formattedStack;
+    string logJson[] = { "\"tid\":", TRUNCATE_TEST_BUNDLE_NAME, "/system/bin/appspawn", "OS_DfxWatchdog" };
+    logJson[0] += std::to_string(g_testPid);
+    len = sizeof(logJson) / sizeof(logJson[0]);
+    count = GetKeywordsNum(formattedStack, logJson, len);
+    EXPECT_EQ(count, len) << formattedStack << "DumpCatcherInterfacesTest031 Failed";
     GTEST_LOG_(INFO) << "DumpCatcherInterfacesTest031: end.";
 }
 #endif
@@ -1048,6 +1057,38 @@ HWTEST_F(DumpCatcherInterfacesTest, DumpCatcherInterfacesTest039, TestSize.Level
     ExecuteCommands(startFaultloggerdCmd);
     EXPECT_TRUE(ret == DUMPCATCH_ECONNECT);
     GTEST_LOG_(INFO) << "DumpCatcherInterfacesTest039: end.";
+}
+
+/**
+ * @tc.name: DumpCatcherInterfacesTest040
+ * @tc.desc: test DumpCatchProcess get user stack
+ * @tc.type: FUNC
+ */
+HWTEST_F(DumpCatcherInterfacesTest, DumpCatcherInterfacesTest040, TestSize.Level2)
+{
+    GTEST_LOG_(INFO) << "DumpCatcherInterfacesTest040: start.";
+    bool isSuccess = g_testPid != 0;
+    if (!isSuccess) {
+        ASSERT_FALSE(isSuccess);
+        GTEST_LOG_(ERROR) << "Failed to launch target hap.";
+        return;
+    }
+    isSuccess = CheckProcessComm(g_testPid, TRUNCATE_TEST_BUNDLE_NAME);
+    if (!isSuccess) {
+        ASSERT_FALSE(isSuccess);
+        GTEST_LOG_(ERROR) << "Error process comm";
+        return;
+    }
+    DfxDumpCatcher dumplog;
+    std::string msg = "";
+    ASSERT_EQ(dumplog.DumpCatchProcess(g_testPid, msg), 0); //user stack
+    string log[] = { "Tid:", "Name:", "#00", "/system/bin/appspawn", "Name:OS_DfxWatchdog" };
+    log[0] += std::to_string(g_testPid);
+    log[1] += TRUNCATE_TEST_BUNDLE_NAME;
+    int len = sizeof(log) / sizeof(log[0]);
+    int count = GetKeywordsNum(msg, log, len);
+    EXPECT_EQ(count, len) << msg << "DumpCatcherInterfacesTest040 Failed";
+    GTEST_LOG_(INFO) << "DumpCatcherInterfacesTest040: end.";
 }
 } // namespace HiviewDFX
 } // namepsace OHOS
