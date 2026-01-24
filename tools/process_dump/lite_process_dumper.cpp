eaf3f211a80495090dc572d389e3440b6dbf2a92
xulong19 <xulong19@huawei.com>
2026-01-24 13:54:39 +0800

    标识是否为arkwebcore
    Signed-off-by: xulong19 <xulong19@huawei.com>
    Change-Id: I47c6c3697f2271c3a2741f0122629d8a332de2f2

--- tools/process_dump/lite_process_dumper.cpp
+++ tools/process_dump/lite_process_dumper.cpp
@@ -361,8 +361,9 @@ bool LiteProcessDumper::Report()
         SysEventReporter reporter(request_.type);
         reporter.Report(*process_, request_);
     }
+    std::string isArkWeb = std::string("isArkWeb:") + (DfxMaps::IsArkWebProc() ? "true" : "false") + ";";
     std::string reason = DfxSignal::PrintSignal(request_.siginfo) + "\n";
-    std::string summary = "litehandler:" + reason + keyThreadStackStr_;
+    std::string summary = "litehandler:" + isArkWeb + reason + keyThreadStackStr_;
 #ifndef HISYSEVENT_DISABLE
     HiSysEventParam params[] = {
         {.name = "PID", .t = HISYSEVENT_INT32, .v = { .i32 = request_.pid}, .arraySize = 0},
