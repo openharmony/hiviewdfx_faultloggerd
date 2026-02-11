52e27b6ea6e71a75466774dfc0b22b7e16261c1f
dong-bosi123 <dongbosi@huawei.com>
2026-02-11 14:21:02 +0800

    取消在processdump里 crash上报时上报PSS_MEMORY
    Signed-off-by: dong-bosi123 <dongbosi@huawei.com>

--- tools/process_dump/reporter.cpp
+++ tools/process_dump/reporter.cpp
@@ -24,10 +24,8 @@
 #include "dfx_process.h"
 #include "dfx_signal.h"
 #include "dfx_thread.h"
-#include "dfx_util.h"
 #include "faultlogger_client_msg.h"
 #include "procinfo.h"
-#include "proc_util.h"
 #include "dfx_maps.h"
 #ifndef HISYSEVENT_DISABLE
 #include "hisysevent.h"
@@ -105,7 +103,6 @@ void CppCrashReporter::ReportToHiview(DfxProcess& process, const ProcessDumpRequ
     info.sectionMaps["PROCESS_RSS_MEMINFO"] = std::to_string(process.GetRss());
     info.sectionMaps["PROCESS_LIFETIME"] = std::to_string(process.GetLifeTime());
     info.sectionMaps["IS_ARKWEB_CORE"] = DfxMaps::IsArkWebProc() ? "true" : "false";
-    info.sectionMaps["PSS_MEMORY"] = std::to_string(IsBetaVersion() ? GetPssMemory() : 0);
     info.sectionMaps["IS_SIG_ACTION"] = request.isSigAction ? "Yes" : "No";
     addFaultLog(&info);
     DFXLOGI("Finish report fault to FaultLogger %{public}s(%{public}d,%{public}d)",
